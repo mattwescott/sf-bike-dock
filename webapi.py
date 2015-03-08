276dc6feb54db078009eb4d6cfcad1cc661a1c53
Nick Hughes <nph123@gmail.com>
2015-03-08 14:57:40 -0700

    API should be working now

--- webapi.py
+++ webapi.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
+from geopy.distance import vincenty
 import pandas as pd
 import boto
 import io
-import os
 import re
 
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 
@@ -20,31 +18,28 @@ def load_data_from_S3():
     #https://s3-us-west-1.amazonaws.com/bike-parking/sf_bike_public_parking.csv
     s3_bucket = 'bike-parking'
     s3_key = 'sf_bike_public_parking.csv'
-
-    if (os.environ.get('AWS_ACCESS_KEY_ID') and
-        os.environ.get('AWS_SECRET_ACCESS_KEY')):
-        # Read credentials from environment variables
-        conn = boto.connect_s3()
-    else:
-        # Assume Nick is running the app, so read credentials from profile
-        conn = boto.connect_s3(profile_name='nick')
-
+    conn = boto.connect_s3()
     bucket = conn.get_bucket(s3_bucket)
+
+    # Read text data from S3 bucket and convert to unicode
     str_data = bucket.get_key(s3_key).get_contents_as_string()
     str_data = io.StringIO(unicode(str_data, 'utf-8'))
 
+    # Read string data into Pandas and parse lat/long values
     df = pd.read_csv(str_data)
-    lat_lng_values = df.COORDINATES.apply(parse_lat_long_values, axis=0)
+    lat_lng_series = df['COORDINATES'].apply(parse_lat_long_values)
+    df['LAT_LNG'] = lat_lng_series
+    return df
 
-def parse_lat_long_values(coord_str, axis):
+def parse_lat_long_values(coord_str):
     RE = r'\((?P<lat>-?\d+\.\d+)\,\s*(?P<lng>-?\d+\.\d+)\)'
     m = re.search(RE, coord_str)
     if m and len(m.groups()) == 2:
         lat, lng = lat_lng_to_numeric(m.group('lat'), m.group('lng'))
-        return (lat, lng)
     else:
         print('Unable to parse coordinates string: {}'.format(coord_str))
-        return (None, None)
+        lat, lng = None, None
+    return (lat, lng)
 
 def lat_lng_to_numeric(lat_str, lng_str):
     try:
@@ -56,37 +51,41 @@ def lat_lng_to_numeric(lat_str, lng_str):
         lat, lng = None, None
     return lat, lng
 
+def lookup_nearest_spots(lat, lng, n):
+    # Compute distance between user lat/lng and each bike parking lat/lng
+    dists = bike_df['LAT_LNG'].apply(lambda x: vincenty(x, (lat, lng)).ft)
 
-@app.route('/')
-def index():
-   return '<h1>Optimally dock your bike with BikeDocker(TM)</h1>'
+    # Sort distances in ascending order and select the first n values
+    dists.sort()
+    idx = dists.index.tolist()[:n]
+
+    # Return entries as JSON
+    return bike_df.iloc[idx].to_json()
 
 
-@app.route('/bike_parking', methods=['GET'])
+@app.route('/api/bike_parking', methods=['GET'])
 def find_nearest_bike_parking():
     # Extract params from URL
     lat_str = request.args.get('lat')
     lng_str = request.args.get('long')
     n = request.args.get('n')
 
-    lat, lng = lat_lng_to_numeric(lat_str, lng_str):
+    # Convert lat/lng to numeric values
+    lat, lng = lat_lng_to_numeric(lat_str, lng_str)
 
     if lat is None or lng is None:
-        return
+        return jsonify({'msg': 'Invalid lat/long data passed'}), 400
 
+    # Convert n to numeric value
     if n is None:
         n = 1
     else:
         try:
             n = int(n)
         except:
-            return None
+            return jsonify({'msg': 'Invalid n value passed'}), 400
 
-    return jsonify(lookup_nearest_spots(lat, lng, n))
-
-
-def lookup_nearest_spots(lat, lng, n):
-
+    return lookup_nearest_spots(lat, lng, n), 200
 
 
 if __name__ == '__main__':
